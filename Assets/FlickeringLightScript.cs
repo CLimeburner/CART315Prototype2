e8c9db618823eeaa0fc83d6d8dfcc1c13ab4bb02
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2022-02-17 00:41:34 -0500

    Added flickering light effects

---
+++ Assets/FlickeringLightScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLightScript : MonoBehaviour
+{
+
+    public Light flickerLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      flickerLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      flickerLight.intensity = Random.Range(0.5f, 1.0f);
+    }
+}
